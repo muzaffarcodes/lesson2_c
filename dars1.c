723ece44eff9e31a0d524fbde678978dbbeabf82
Muzaffar Khaydarov <114870653+muzaffarcodes@users.noreply.github.com>
2024-01-03 21:36:25 +0500

    Add files via upload

---
+++ dars1.c
@@ -0,0 +1,362 @@
+#include <stdio.h> // STANDART INPUT OUTPUT .HEADER
+//1
+int main()
+{
+    /*
+    {} CURLY BRACKETS
+    () PARANTHESES
+    /  SLASH
+    \  BACKSLASH
+    ;  SEMICOLON
+    .  DOT
+    <> DIAMOND BRACKETS
+    !  BANG SYMBOL
+    "  DOUBLE QUOTE
+    ,  COMMA
+    */
+
+    printf("Hello world!\n");
+    printf("Sen aqlsizsan!\n");
+    printf("Ey bola...\n");
+
+    return 0;
+}
+
+#include <stdio.h> // STANDART INPUT OUTPUT .HEADER
+//2
+int main()
+{
+    /*
+    { } CURLY BRACKETS
+    ( ) PARANTHESES
+    /   SLASH
+    \   BACKSLASH
+    ;   SEMICOLON
+    .   DOT
+    ,   COMMA
+    < > DIMOND BRACKETS
+    !   BANG SYMBOL
+    "   DOUBLE QUOTE
+    */
+
+    printf("JCH-2022\n");
+    printf("Saudiya 3 - 0 O'zbekiston!\n");
+    printf("Biz jahon chempionatiga chiqamiz, albatta\n ");
+    // CHAR
+    char belgi = '1'; // 1 BYTE
+    printf("%c\n",belgi); // c -> char
+    char belgi1 = 'a'; // 1 BYTE
+    printf("Belgi: %c",belgi1);
+    // CHAR SERIYA
+    char ism[8] = "John"; // ...BYTE
+    printf("\n%s\n",ism); // s -> seriya
+
+    char familya[] = "Wick";
+    printf("Inputda ishlamaydi: %s\n", familya);
+    //INTEGER
+    int butunSon = 2147483647; // 4BYTE // 2.147.483.647
+    printf("Butun Son: %d \n", butunSon); // d -> decimal
+    //FLOAT
+    float qoldiqSon = 6.3; // 4BYTE
+    printf("Qoldiq Sin: %.1lf\n",qoldiqSon); // f -> float
+    // DOUBLE
+    double kattaHajmQoldiqSon = 69.7; // 8BYTE
+    printf("Katta Qoldiq Son: %.1lf\n",kattaHajmQoldiqSon); //lf -> long decimal
+    return 0;
+}
+
+#include <stdio.h> // STANDART INPUT OUTPUT .HEADER
+//3
+int main()
+{
+    /*
+    {} CURLY BRACKETS
+    () PARANTHESES
+    / SLASH
+    \ BACKSLASH
+    ; SEMICOLON
+    . DOT
+    <> DIAMOND BRACKETS
+    ! BANG SYMBOL
+    " DOUBLE QUOTE
+    , COMMA
+    */
+
+    printf("Salom\n");
+    printf("Hayr\n");
+    printf(Isming nima?\n);
+    //CHAR
+    char belgi = '1'; // 1BYTE
+    printf("%c\n",belgi); // c -> char
+    char belgi1 = 'a'; // 1BYTE
+    printf("Belgi: %c",belgi1);
+    //CHAR SERIYA
+    char ism[8] = "John"; //...BYTE
+    printf("\n%s\n",ism); // s -> seriya
+
+    char famliya[] = "Wick";
+    printf("Inputda ishlamaydi: $s\n",familya);
+    //INTEGER
+    int butunSon = 2147483647; // 4BYTE // 2.147.483.647
+    printf("Butun Son: %d \n", butunSon); // d -> decimal
+    //FLOAT
+    float qoldiqSon = 21.12; // 4BYTE
+    printf("Butun Son: %,lf\n", qoldiqSon); // f -> float
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 33.21;  // lf -> long float
+    return 0;
+}
+
+#include <stdio.h>
+//4
+int main()
+{
+    /*
+    {} CURLY BRACKETS
+    () PARANTHESES
+    / SLASH
+    \ BACK SLASH
+    . DOT
+    ; SEMICOLON
+    <> DIAMOND BRACKETS
+    ! BANG SYMBOL
+    " DOUBLE QUOTE
+    , COMMA
+    */
+
+    printf("Namaste!\n");
+    printf("Raundeuter!\n");
+    printf("Konichua!\n");
+    //CHAR
+    char belgi = '1'; //1BYTE
+    printf("%c\n" ,belgi); // c -> char
+    char belgi1 = '3'; //1BYTE
+    printf(Belgi: "%c\n ",belgi1);
+    //CHAR SERIYA
+    char ism[22] = "Zemeister"; // ...BYTE
+    printf("\n%s\n", ism); //s -> seriya
+
+    char familya[] = "Saud";
+    printf("Inputda ishlaydi: %s\n", familya);
+    //INTEGER
+    int butunSon = 2147483647; // 4BYTE // 2.147.483.647
+    printf("Butun Son: %d \n", butunSon); // d -> decimal
+    //FLOAT
+    float qoldiqSon = 21.12; // 4BYTE
+    printf("Butun Son: %,lf\n", qoldiqSon); // f -> float
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 33.21;  // lf -> long float
+    return 0;
+}
+
+#include <stdio.h> // STANDARD INPUT OUTPUT .HEADER
+//5
+int main()
+{
+    /*
+    {} CURLY BRACKETS
+    () PARANTHESES
+    / SLASH
+    \ BACKSLASH
+    ; SEMICOLON
+    . DOT
+    <> DIAMOND BRACKETS
+    ! BANG SYMBOL
+    " DOUBLE QUOTE
+    , COMMA
+    */
+
+    printf("Hayr!\n")
+    printf("Hey,SIRI!\n");
+    printf("Privet,Alisa!\n");
+    //CHAR
+    char belgi = '1'; //1BYTE
+    printf("%c\n",belgi); // c -> char
+    char belgi1 = 'a';
+    printf("Belgi: %c",belgi1);
+    //CHAR SERIYA
+    char ism[8] = "Islom"; //...BYTE
+    printf("\n%s\n",ism); // s -> seriya
+
+    char familya[] = "Karimov";
+    printf("Tug'ilgan yil: 1938-yil %s\n", familya);
+    //INTEGER
+    int butunSon = 2147483647; // 4BYTE // 2.147.483.647
+    printf("Butun Son: %d \n", butunSon); // d -> decimal
+    //FLOAT
+    float qoldiqSon = 6.3; //4BYTE
+    printf("Qoldiq Son: %,1f\n", qoldiqSon); // f -> float
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 69.7; // 8BYTE
+    printf("Katta Qoldiq Son: %,1lf\n", kattaHajmliQoldiqSon); // lf -> long float
+    return 0;
+}
+
+#include <stdio.h> // STANDARD INPUT OUTPUT .HEADER
+//6
+int main()
+{
+    /*
+    {} CURLY BRACKETS
+    () PARANTHESES
+    / SLASH
+    \ BACKSLASH
+    ; SEMICOLON
+    , COMMA
+    . DOT
+    <> DIAMOND BRACKETS
+    ! BANG SYMBOL
+    */
+
+    printf("Gracias!\n");
+    printf("Wonderful!\n");
+    printf("Stupid boy!\n");
+    //CHAR
+    char belgi = '3'; //1BYTE
+    printf("%c\n",belgi); //c-> char
+    char belgi1 = 'q'; //1BYTE
+    printf("Belgi: %c", belgi1);
+    //CHAR SERIYA
+    char ism[9] = "Ahad"; // ... BYTE
+    printf("\n%s\n", ism); // s-> seriya
+
+    char familya[] = "Qayum";
+    printf("Oskarni yaqinda oladi: %s\n", familya);
+    //INTEGER
+    int butunSon = 2127483647; // 4BYTE // 2.127.483.647
+    printf("Butun Son: %d \n", butunSon); // d -> decimal
+    //FLOAT
+    float qoldiqSon = 4.5; // 4BYTE
+    printf("Qoldiq SOn: %.1f\n", qoldiqSon);
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 77.1; //8BYTE
+    printf("Katta Qoldiq Son: %.1lf\n", kattaHajmliQoldiqSon);
+    return 0;
+}
+
+#include <stdio.h> //STANDART INPUT OUTPUT >HEADER
+//7
+int main()
+{
+    printf("Hey,Google!\n");
+    printf("Hey,MJAY!\n");
+    printf("Racks on racks!\n");
+    //CHAR
+    char belgi = '2';
+    printf("%c\n", belgi);
+    char belgi1 = 'l';
+    printf("Belgi: %c", belgi1);
+    //CHAR SERIYA
+    char ism[16] = "Arofat";
+    printf("\n%s\n", ism); // s-> seriya
+    char familya[] = "Aliiyeva";
+    printf("Bu uning ismi: %s\n", familya);
+    //INTEGER
+    int butunSon = 2147483647; // 4BYTE
+    printf("Butun Son: %d \n", butunSon);
+    //FLOAT
+    float qoldiqSon = 3.4;
+    printf("Qoldiq Son: %.1f\n", qoldiqSon);
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 44,3 // 8BYTE
+    printf("Katta Hajmli Qoldiq Son: %.1lf\n",kattaHajmliQoldiqSon);
+    return 0;
+}
+
+#include <stdio.h>
+//8
+int main()
+{
+    printf("Telegram.com\n");
+    printf("Whatsapp!\n");
+    printf("Facebook\n");
+    //CHAR
+    char belgi = '7';
+    printf("%c\n," belgi);
+    char belgi2 = 'r';
+    printf("Belgi: %c", belgi2);
+    //CHAR SERIYA
+    char ism[12] = "Sarvinoz";
+    printf("\n%s\n",ism);
+
+    char familya[] = "Mamadaliyeva";
+    printf("Isming nima: %s\n", familya);
+    //INTEGER
+    int butunSon = 2147483646; //2.147.483.646
+    printf("Butun Son: %d\n", butunSon);
+    //FLOAT
+    float qoldiqSon = 6.1;
+    printf("Qoldiq Son: %.1f\n", qoldiqSon);
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 99,2;
+    printf("Katta Hajmli Qoldiq Son: %.1lf\n", kattaHajmliQoldiqSon);
+    return 0;
+}
+
+#include <stdio.h>
+//9
+int main()
+{
+    printf("Hey,Amazon\n");
+    printf("Hey,Bliss\n");
+    printf("Hi,Girls\n");
+    //CHAR
+    char belgi = '5';
+    printf("%c\n", belgi);
+    char belgi3 = 's';
+    printf("Belgi: %c\n", belgi3);
+
+    //CHAR SERIYA
+    char ism[7] = "Sardor";
+    printf("\n%s\n", ism);
+
+    char familya[] = "Mansurkhojaev";
+    printf("Familyangiz: \n%s\n");
+
+    //INTEGER
+    int butunSon = 2147432246; //2.147.432.246
+    printf("Butun Son: %d\n", butunSon);
+    //FLOAT
+    float qoldiqSon = 7.1;
+    printf("Qoldiq Son: %.1f\n", qoldiqSon);
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 344,2;
+    printf("Katta Hajmli Qoldiq Son: %.1lf\n", kattaHajmliQoldiqSon);
+    return 0;
+
+#include <stdio.h>
+//10
+int main()
+{
+    printf("Hey,boys!\n");
+    printf("I'am in the right place!\n");
+    printf("You are daft!\n");
+
+    //CHAR
+    char belgi = '7';
+    printf("%c\n",belgi);
+
+    char belgi4 = 'h';
+    printf("\n%c\n", belgi4);
+
+    //CHAR SERIYA
+    char ism[11] = "Muhammadali";
+    printf("\n%s\n", ism);
+
+    char familya[] = "Haydarov";
+    printf("NImadir...: \n%s\n", familya);
+
+    //INTEGER
+    int butunSon = 11;
+    printf("Butun Son: %d\n", butunSon);
+
+    //FLOAT
+    float qoldiqSon = 3,4;
+    printf("Qoldiq Son: %.1f\n");
+    //DOUBLE
+    double kattaHajmliQoldiqSon = 443,3;
+    printf(:"Katta Hajmli Qoldiq Son: \n%.1lf\n");
+    return 0;
+}
+}
+
